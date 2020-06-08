5d030e969a0a1fbb62bf5e8597d395d71119ba93
amosborn <amosborn@mail.com>
2020-06-08 20:21:57 +0000

    Login required added to logout, redirect added to login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from accounts.forms import UserLoginForm
 
 
@@ -11,6 +12,7 @@ def index(request):
     return render(request, 'index.html')
 
 
+@login_required
 def logout(request):
     """Log user out"""
     auth.logout(request)
@@ -20,6 +22,8 @@ def logout(request):
 
 def login(request):
     """Return login page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
 
@@ -30,6 +34,7 @@ def login(request):
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully logged in.")
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None,
                                      "Username or password is incorrect.")
