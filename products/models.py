3469e2e856d599fa0606df894a6ef4ea1c6c3623
amosborn <amosborn@mail.com>
2020-06-18 07:08:09 +0000

    Created Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=254, default='')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to='images')
+    auction_done = models.BooleanField()
+    auction_time = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
