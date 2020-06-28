eb88e1f4141362244370559a41811ed54f751868
amosborn <amosborn@mail.com>
2020-06-28 10:19:48 +0000

    Docstring added to search view

--- search/views.py
+++ search/views.py
@@ -3,5 +3,6 @@ from products.models import Product
 
 
 def do_search(request):
+    """Search for products by product name"""
     products = Product.objects.filter(name__icontains=request.GET['q'])
     return render(request, "products.html", {"products": products})
