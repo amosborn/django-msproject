f9db1c4dcc7d19597abf453468e32438f2509d9d
amosborn <amosborn@mail.com>
2020-07-02 15:36:40 +0000

    Contexts created for storing items added to cart

---
+++ cart/contexts.py
@@ -0,0 +1,24 @@
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
+
+def cart_contents(request):
+    """Ensures cart contents available on every page"""
+    cart = request.session.get('cart', {})
+
+    cart_items = []
+    total = 0
+    product_count = 0
+
+    for id, quantity in cart.items():
+        if quantity is None:
+            q = 0
+        else:
+            q = quantity
+        product = get_object_or_404(Product, pk=id)
+        total += q * product.price
+        product_count += q
+        cart_items.append({'id': id, 'quantity': q, 'product': product})
+
+    return {'cart_items': cart_items, 'total': total,
+            'product_count': product_count}
