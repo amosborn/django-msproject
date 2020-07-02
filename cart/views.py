a950597e2c2c2a2a2690b73cfaa2212c19209eea
amosborn <amosborn@mail.com>
2020-07-02 15:44:27 +0000

    View to add products to cart

--- cart/views.py
+++ cart/views.py
@@ -6,3 +6,18 @@ from django.shortcuts import render, redirect, reverse
 def view_cart(request):
     """Renders cart contents page"""
     return render(request, 'cart.html')
+
+
+def add_to_cart(request, id):
+    """Add a quantity of a product to the cart"""
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+    if id in cart:
+        if cart[id] is not None:
+            cart[id] = int(cart[id]) + quantity
+    else:
+        cart[id] = cart.get(id, quantity)
+
+    request.session['cart'] = cart
+    return redirect(reverse('index'))
