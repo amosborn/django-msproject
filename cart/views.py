ff52c988a9c092b1c9f9d985f10fe5263d91e0cf
amosborn <amosborn@mail.com>
2020-07-02 15:53:08 +0000

    View to adjust quantities in cart

--- cart/views.py
+++ cart/views.py
@@ -21,3 +21,18 @@ def add_to_cart(request, id):
 
     request.session['cart'] = cart
     return redirect(reverse('index'))
+
+
+def adjust_cart(request, id):
+    """Adjust quantity of the selected product"""
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[id] = quantity
+    else:
+        cart.pop(id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
