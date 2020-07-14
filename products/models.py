325a52fef94ab74bb283b961e6f9d0c0565abafe
amosborn <amosborn@mail.com>
2020-07-14 07:54:33 +0000

    Removed auction fields from shop products model

--- products/models.py
+++ products/models.py
@@ -6,8 +6,6 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     image = models.ImageField(upload_to='images')
-    auction_done = models.BooleanField()
-    auction_time = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.name
