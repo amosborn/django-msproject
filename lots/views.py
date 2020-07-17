ca4e31de79c9ab30a1cabad5461f7fe0d8babde9
amosborn <amosborn@mail.com>
2020-07-17 22:07:44 +0000

    Created view to display all lots

--- lots/views.py
+++ lots/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Lot
 
 # Create your views here.
+
+
+def all_lots(request):
+    lots = Lot.objects.all()
+    return render(request, 'lots.html', {'lots': lots})
