ba4daf569bb8feb365c65d0e99691ae2f2f283b5
amosborn <amosborn@mail.com>
2020-07-17 22:06:57 +0000

    Created model for auction lots

--- lots/models.py
+++ lots/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Lot(models.Model):
+    """Auction lot"""
+    name = models.CharField(max_length=254, default='')
+    description = models.TextField()
+    image = models.ImageField(upload_to='images')
+    auction_done = models.BooleanField(default=False)
+    auction_end_time = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
