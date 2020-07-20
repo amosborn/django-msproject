d9664f9036f6c6c6e40a18660e6439165fa4fcb6
amosborn <amosborn@mail.com>
2020-07-20 13:33:42 +0000

    Url added to open lot detail page

--- lots/urls.py
+++ lots/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import all_lots
+from .views import all_lots, lot_detail
 
 urlpatterns = [
     url(r'^$', all_lots, name='lots'),
+    url(r'^(?P<pk>\d+)/$', lot_detail, name='lot_detail'),
 ]
