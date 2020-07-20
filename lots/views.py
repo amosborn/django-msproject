931eba082a1fcefb19b3562520208d02179a3694
amosborn <amosborn@mail.com>
2020-07-20 13:31:29 +0000

    Added view to return page with individual lolot detail

--- lots/views.py
+++ lots/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404
 from .models import Lot
 
 # Create your views here.
 
 
 def all_lots(request):
+    """Return all lots"""
     lots = Lot.objects.all()
     return render(request, 'lots.html', {'lots': lots})
+
+
+def lot_detail(request, pk):
+    """Return individual lot object and render it to lotdetail.html page"""
+    lot = get_object_or_404(Lot, pk=pk)
+    return render(request, "lotdetail.html", {'lot': lot})
