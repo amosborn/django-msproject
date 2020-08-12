bd2ff7fba724f695e7c2fdbf3ba5918752b7a454
amosborn <amosborn@mail.com>
2020-08-12 23:47:04 +0000

    View for past auction results added

--- lots/views.py
+++ lots/views.py
@@ -1,16 +1,31 @@
-from django.shortcuts import render,  get_object_or_404
-from .models import Lot
+from django.shortcuts import render, redirect,  get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import datetime
+from django.contrib import auth
+from .models import Lot, Bid
+from .forms import BidForm
+
 
 # Create your views here.
 
 
-def all_lots(request):
-    """Return all lots"""
-    lots = Lot.objects.all()
+def all_current_lots(request):
+    """Return all current auction lots"""
+    now = timezone.now()
+    lots = Lot.objects.filter(auction_end_time__gte=now)
     return render(request, 'lots.html', {'lots': lots})
+
+
+def all_past_lots(request):
+    """Return all past auction lots"""
+    now = timezone.now()
+    pastlots = Lot.objects.filter(auction_end_time__lt=now)
+    return render(request, 'pastlots.html', {'pastlots': pastlots})
 
 
 def lot_detail(request, pk):
     """Return individual lot object and render it to lotdetail.html page"""
     lot = get_object_or_404(Lot, pk=pk)
-    return render(request, "lotdetail.html", {'lot': lot})
+    return render(request, 'lotdetail.html', {'lot': lot})
