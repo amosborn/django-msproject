41c9c17f64321007ea443336028fd57658fab251
amosborn <amosborn@mail.com>
2020-08-13 20:29:21 +0000

    Register auction and bid models with admin

--- lots/admin.py
+++ lots/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Lot
+from .models import Lot, Bid, Auction
 
 
 admin.site.register(Lot)
+admin.site.register(Bid)
+admin.site.register(Auction)
