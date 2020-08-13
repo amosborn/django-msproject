b283606c8a4c1c23ba3823d29a22e56e4f4ce9ce
amosborn <amosborn@mail.com>
2020-08-13 20:26:50 +0000

    Added Auction model

--- lots/models.py
+++ lots/models.py
@@ -15,6 +15,19 @@ class Lot(models.Model):
         return self.name
 
 
+class Auction(models.Model):
+    """Auction model"""
+    lot = models.ForeignKey(Lot, on_delete=models.CASCADE)
+    number_of_bids = models.IntegerField(default=0)
+    winning_bidder = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    winning_bid = models.IntegerField(default=0)
+    auction_end_time = models.DateTimeField(blank=True, null=True)
+    paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "Auction: " + str(self.pk) + " Lot: " + str(self.lot.name)
+
+
 class Bid(models.Model):
     """Auction bid model"""
     user = models.ForeignKey(User, on_delete=models.CASCADE)
