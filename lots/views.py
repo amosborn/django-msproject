dcc8689c9cf310f806ac41b831812ec2db5bd2db
amosborn <amosborn@mail.com>
2020-08-14 23:36:13 +0000

    View for auction page with bid form and create default bid

--- lots/views.py
+++ lots/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from datetime import datetime
 from django.contrib import auth
-from .models import Lot, Bid
+from .models import Lot, Bid, Auction
 from .forms import BidForm
 
 
@@ -29,3 +29,26 @@ def lot_detail(request, pk):
     """Return individual lot object and render it to lotdetail.html page"""
     lot = get_object_or_404(Lot, pk=pk)
     return render(request, 'lotdetail.html', {'lot': lot})
+
+
+@login_required
+def auction(request, auction_id):
+    """Render auction and bidding page"""
+
+    auction = get_object_or_404(Auction, id=auction_id)
+    bid = Bid.objects.filter(auction=auction_id).order_by('-bid_time')
+    bid_form = BidForm
+    
+    if auction:
+        if bid:
+            latest_bid = bid[0]
+        else:
+            bid_default = Bid()
+            bid_default.user = get_object_or_404(User, id=1)
+            bid_default.auction = auction
+            bid_default.bid_amount = 0.00
+            bid_default.save()
+            latest_bid = bid_default
+
+    return render(request, 'bidform.html', {'auction': auction,
+                  'latest_bid': latest_bid, 'bid_form': bid_form})
