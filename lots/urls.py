76ecc65a8cb8da8926294bf4440388d0c7e37b48
amosborn <amosborn@mail.com>
2020-08-19 22:00:45 +0000

    Past lots url amended to auctions

--- lots/urls.py
+++ lots/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from .views import all_current_lots, all_past_lots, lot_detail, auction
+from .views import all_current_lots, all_past_lots, lot_detail, auction, bid
 
 urlpatterns = [
     url(r'^$', all_current_lots, name='lots'),
-    url(r'^pastlots/$', all_past_lots, name='past_lots'),
+    url(r'^pastauctions/$', all_past_lots, name='past_lots'),
     url(r'^(?P<pk>\d+)/$', lot_detail, name='lot_detail'),
     url(r'^(?P<auction_id>\d+)/edit/$', auction, name='auction'),
 ]
