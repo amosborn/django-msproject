f49bb6ccbebbf01bc031f6c29c0046ff9693ca2f
amosborn <amosborn@mail.com>
2020-08-21 21:08:17 +0000

    Url to lot detail amended for lot_id

--- lots/urls.py
+++ lots/urls.py
@@ -4,6 +4,6 @@ from .views import all_current_lots, all_past_lots, lot_detail, auction, bid
 urlpatterns = [
     url(r'^$', all_current_lots, name='lots'),
     url(r'^pastauctions/$', all_past_lots, name='past_lots'),
-    url(r'^(?P<pk>\d+)/$', lot_detail, name='lot_detail'),
+    url(r'^(?P<lot_id>\d+)/$', lot_detail, name='lot_detail'),
     url(r'^(?P<auction_id>\d+)/edit/$', auction, name='auction'),
 ]
