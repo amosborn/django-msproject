8d8732c998802165876bfffa32a0a42f6e5abc28
amosborn <amosborn@mail.com>
2020-08-25 13:50:08 +0000

    Added filter to search current lots only

--- search/views.py
+++ search/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from products.models import Product
 from lots.models import Lot
+from django.utils import timezone
 
 
 def lot_search(request):
     """Search for products by product name"""
-    lots = Lot.objects.filter(name__icontains=request.GET['q'])
+    now = timezone.now()
+    lots = Lot.objects.filter(auction_end_time__gte=now,
+                              name__icontains=request.GET['q'])
     return render(request, "lots.html", {"lots": lots})
 
 
