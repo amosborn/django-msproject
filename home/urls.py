4664d8a828e7e6b2908ce17a0b26100889e6a3c4
amosborn <amosborn@mail.com>
2020-08-26 13:51:54 +0000

    View and url to home/index.html as home page

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import about
+from .views import about, index
 
 urlpatterns = [
-    url(r'^$', about, name='about'),
+    url(r'^$', index, name='index'),
+    url(r'^about/$', about, name='about'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def index(request):
+    """Display home page"""
+    return render(request, 'index.html')
+
+
 def about(request):
     """Display the about.html page"""
     return render(request, 'about.html')
