c5245f75fbea55b67e095447724749ee45d19e6c
amosborn <amosborn@mail.com>
2020-08-26 13:54:25 +0000

    Url name lots changed to current_lots

--- lots/urls.py
+++ lots/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import all_current_lots, all_past_lots, lot_detail, auction, bid
 
 urlpatterns = [
-    url(r'^$', all_current_lots, name='lots'),
+    url(r'^$', all_current_lots, name='current_lots'),
     url(r'^pastauctions/$', all_past_lots, name='past_lots'),
     url(r'^(?P<lot_id>\d+)/$', lot_detail, name='lot_detail'),
     url(r'^(?P<auction_id>\d+)/edit/$', auction, name='auction'),
