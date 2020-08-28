fc2ab921faf569ddcd2d3d9582b6f66104b119bb
amosborn <amosborn@mail.com>
2020-08-28 09:01:15 +0000

    Cart checkout name amended and auction checkout url added

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import checkout
+from .views import checkout_cart, checkout_auction
 
 
 urlpatterns = [
-    url(r'^$', checkout, name='checkout'),
+    url(r'^$', checkout_cart, name='checkout_cart'),
+    url(r'^(?P<auction_id>\d+)/$', checkout_auction, name='checkout_auction')
 ]
