a115a3709c32a783dfa9a543ad71dee5c6729c4a
amosborn <amosborn@mail.com>
2020-08-28 08:19:54 +0000

    Create and save user functions separated

--- accounts/models.py
+++ accounts/models.py
@@ -22,8 +22,10 @@ class Profile(models.Model):
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    """Create and update user profile"""
     if created:
         Profile.objects.create(user=instance)
 
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
