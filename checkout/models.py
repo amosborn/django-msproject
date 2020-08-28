729c5378fbd3159175216e8e7d4d9670d84e47a1
amosborn <amosborn@mail.com>
2020-08-28 08:36:50 +0000

    Auction added to order line item

--- checkout/models.py
+++ checkout/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from products.models import Product
+from lots.models import Auction
 
 # Create your models here.
 
@@ -21,9 +22,10 @@ class Order(models.Model):
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False)
-    product = models.ForeignKey(Product, null=False)
+    product = models.ForeignKey(Product, null=True)
+    auction = models.ForeignKey(Auction, null=True)
     quantity = models.IntegerField(blank=False)
 
     def __str__(self):
         return "{0} {1} @ {2}".format(
-            self.quantity, self.product.name, self.product.price)
+            self.id, self.order.date, self.order.full_name)
