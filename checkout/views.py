6968633cf6cabb6090e64b2618f2c0fcb32012c1
amosborn <amosborn@mail.com>
2020-08-30 19:46:00 +0000

    Order form prepopulate moved to try except block and added to auction checkout

--- checkout/views.py
+++ checkout/views.py
@@ -19,20 +19,7 @@ stripe.api_key = settings.STRIPE_SECRET
 def checkout_cart(request):
     profile = Profile.objects.get(user=request.user)
     payment_form = MakePaymentForm(request.POST)
-    if profile:
-        order_form = OrderForm(initial={
-            'full_name': profile.full_name,
-            'email': profile.user.email,
-            'phone_number': profile.phone_number,
-            'country': profile.country,
-            'postcode': profile.postcode,
-            'town_or_city': profile.town_or_city,
-            'street_address1': profile.street_address1,
-            'street_address2': profile.street_address2,
-            'county': profile.county,
-        })
-    else:
-        order_form = OrderForm(request.POST)
+    order_form = OrderForm(request.POST)
 
     if request.method == 'POST':
 
@@ -75,9 +62,24 @@ def checkout_cart(request):
                 print(payment_form.errors)
                 messages.error(request,
                                "We were unable to take payment with this card")
-        else:
-            payment_form = MakePaymentForm()
-            order_form = OrderForm()
+
+    try:
+        profile = Profile.objects.get(user=request.user)
+        order_form = OrderForm(initial={
+            'full_name': profile.full_name,
+            'email': profile.user.email,
+            'phone_number': profile.phone_number,
+            'country': profile.country,
+            'postcode': profile.postcode,
+            'town_or_city': profile.town_or_city,
+            'street_address1': profile.street_address1,
+            'street_address2': profile.street_address2,
+            'county': profile.county,
+        })
+    except Profile.DoesNotExit:
+        order_form = OrderForm()
+
+        payment_form = MakePaymentForm()
 
     return render(request, "checkout.html",
                   {"order_form": order_form,
@@ -124,9 +126,23 @@ def checkout_auction(request, auction_id):
                 messages.error(request,
                                "Unable to take payment with this card")
 
-        else:
-            payment_form = MakePaymentForm()
-            order_form = OrderForm()
+    try:
+        profile = Profile.objects.get(user=request.user)
+        order_form = OrderForm(initial={
+            'full_name': profile.full_name,
+            'email': profile.user.email,
+            'phone_number': profile.phone_number,
+            'country': profile.country,
+            'postcode': profile.postcode,
+            'town_or_city': profile.town_or_city,
+            'street_address1': profile.street_address1,
+            'street_address2': profile.street_address2,
+            'county': profile.county,
+        })
+    except Profile.DoesNotExit:
+        order_form = OrderForm()
+
+        payment_form = MakePaymentForm()
 
     return render(request, "checkout.html",
                   {'auction': auction,
